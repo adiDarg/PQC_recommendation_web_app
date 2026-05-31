9d922b9a873b07bda03245e7756806edc9e6c0bf
Adi Dargatsky <adidargatsky144@gmail.com>
2026-05-31 13:18:06 +0300

    Attempt to fix analytics & speed insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
-    <Analytics>
-        <SpeedInsights>
-            <App />
-        </SpeedInsights>
-    </Analytics>
+    <>
+        <App />
+        <Analytics />
+        <SpeedInsights />
+    </>
 )
